e144c2ecc725713f3f58404f19b3caf64a86c77f
Karthik Prabhu <k.karthik.prabhu@gmail.com>
2013-07-04 14:42:40 +0530

    Changed settings. Tests will now run in sqlite DB

--- newstrolley/settings.py
+++ newstrolley/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.' + localsettings.DB_ENGINE, # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': localsettings.DB_NAME,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': localsettings.DB_USER,
@@ -24,7 +24,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = localsettings.ALLOWED_HOSTS
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
---
+++ nt_test.py
@@ -0,0 +1,30 @@
+from django.test.utils import setup_test_environment, teardown_test_environment
+from django.test.simple import DjangoTestSuiteRunner
+import os
+
+def test_app(app_name, runner):
+	print "Testing app '%s'" % str(app_name)
+	if runner.run_tests([app_name]) == 0:
+		print "Test successful"
+	else:
+		print "Error in app '%s'. Rectify the errors and start again" % str(app_name)
+		exit(1)
+
+def run_tests():
+	setup_test_environment()
+	runner = DjangoTestSuiteRunner()
+	
+	app_order = ["accounts", "newsreader", "tags"]
+	for app in app_order:
+		test_app(app, runner)
+
+	response = raw_input("Testing 'feeds' app will take a while. Would you like to continue? (yes/no): ")
+	if response == "yes":
+		test_app(app, runner)
+
+	teardown_test_environment()
+	path = os.getcwd() + "/newsreader/static/newsreader/media/tab_thumbs/small_image.jpg"
+	if os.path.isfile(path):
+		os.remove(path)
+
+	exit(0)
